0a85f57694eaa87758cd689e52c509e5aa40ae01
Carson Smith <smithcarson1290@gmail.com>
2024-08-28 18:39:34 -0500

    initial commit, complete project 1

---
+++ SalaryCalculator/src/SalaryCalculator/SalaryCalculator.java
@@ -0,0 +1,41 @@
+package SalaryCalculator;
+
+// Project 1 - Salary Calculator - Carson Smith.
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+
+    // instance variables
+    private double startingSalary;
+    private double annualIncreaseRate;
+
+    // constructor
+    public SalaryCalculator(double startingSalary, double annualIncreaseRate) {
+        this.startingSalary = startingSalary;
+        this.annualIncreaseRate = annualIncreaseRate;
+    }
+
+    public double raiseCalc(int years) {
+        return startingSalary * Math.pow(1 + annualIncreaseRate, years);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your starting salary: ");
+        double startingSalary = sc.nextDouble();
+        System.out.println("Enter annual increase rate: ");
+        double annualIncreaseRate = sc.nextDouble();
+
+        SalaryCalculator calculator = new SalaryCalculator(startingSalary, annualIncreaseRate);
+
+        double yearOneSalary = calculator.raiseCalc(1);
+        double yearTwoSalary = calculator.raiseCalc(2);
+
+        System.out.println("After the first year your salary will be $" + yearOneSalary);
+        System.out.println("After the second year your salary will be $" + yearTwoSalary);
+
+        sc.close();
+    }
+}
