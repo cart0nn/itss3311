a4e48e0c7529da579e3267971c73127a0afc6122
Carson Smith <smithcarson1290@gmail.com>
2024-09-04 17:25:51 -0500

    commenting

--- SalaryCalculator/src/SalaryCalculator/SalaryCalculator.java
+++ SalaryCalculator/src/SalaryCalculator/SalaryCalculator.java
@@ -15,27 +15,30 @@ public class SalaryCalculator {
         this.startingSalary = startingSalary;
         this.annualIncreaseRate = annualIncreaseRate;
     }
-
+    // calculate yearly salaries for int years according to P * (1 + r)^t formula for compound interest
     public double raiseCalc(int years) {
         return startingSalary * Math.pow(1 + annualIncreaseRate, years);
     }
-
+    // handling scanner
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // object creation
 
         System.out.println("Enter your starting salary: ");
-        double startingSalary = sc.nextDouble();
+        double startingSalary = sc.nextDouble(); // takes input as salary. accepts commas and decimal points in number
         System.out.println("Enter annual increase rate: ");
-        double annualIncreaseRate = sc.nextDouble();
+        double annualIncreaseRate = sc.nextDouble(); // takes next input as increase rate, in decimal format
 
-        SalaryCalculator calculator = new SalaryCalculator(startingSalary, annualIncreaseRate);
+        SalaryCalculator calculator = new SalaryCalculator(startingSalary, annualIncreaseRate); // create SalaryCalculator object and pass user input into it
 
+        // calculate raises
         double yearOneSalary = calculator.raiseCalc(1);
         double yearTwoSalary = calculator.raiseCalc(2);
 
+        // output formatted to round numbers to second decimal place
         System.out.printf("After the first year your salary will be $%.2f\n", yearOneSalary);
         System.out.printf("After the second year your salary will be $%.2f", yearTwoSalary);
 
+        // close scanner
         sc.close();
     }
 }
